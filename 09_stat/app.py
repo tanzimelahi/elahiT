dd17c96554ec3f171079c6fcdf9279e95b27da21
newStuyKey <telahi00@stuy.edu>
2019-09-24 00:14:23 -0400

    9 fixed

--- 09_stat/app.py
+++ 09_stat/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 @app.route('/')
+def foo():
+	return "hello"
+
+@app.route("/template")
 def letsGo():
-    return render_template("foo.html",collection=[1,2,34,5])
+    return render_template("foo.html",foo='foooo',collection=[1,2,34,5])
 if __name__=='__main__':
-     app.run(degbug==True)
+	app.debug = True
+	app.run()
